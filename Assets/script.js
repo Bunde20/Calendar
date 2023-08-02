cbcfd6c4c7a0e7655f0be99e6821989712059c6f
Steven Bunde <stevenbunde2020@gmail.com>
2023-08-02 11:20:26 -0400

    set up localStorage

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,29 @@
 $(function () {
   // Current Day
-  var today = dayjs().format("dddd, MMMM D ");
-  $("#currentDay").append(today);
-
   var timeBlock = $(".time-block")
+  var save = $(".saveBtn")
+
+  $("#currentDay").append(today);
+  var today = dayjs().format("dddd, MMMM D ");
   var currentHour = dayjs().hour()
 
-  timeBlock.each(function() {
+  timeBlock.each(function () {
     var blockValue = $(this).attr("id").slice(5)
-    if(blockValue < currentHour) {
+    if (blockValue < currentHour) {
       $(this).children(".description").attr("class", "col-8 col-md-10 description past")
     } else if (blockValue > currentHour) {
       $(this).children(".description").attr("class", "col-8 col-md-10 description future")
     } else {
       $(this).children(".description").attr("class", "col-8 col-md-10 description present")
     }
+  })
+
+  save.on("click", function (event) {
+    event.preventDefault()
+    var key = $(this).parent().attr("id")
+    var value = $(this).siblings(".description").val()
+  
+     localStorage.setItem(key,value )
   })
 
 });
