c02398967aa47a38a9bf1bd1da3d951612ef3741
Steven Bunde <stevenbunde2020@gmail.com>
2023-08-03 18:22:40 -0400

    Added js comments for clarification

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,15 @@
 $(function () {
-  // Current Day
+  // Set global variables
   var timeBlock = $(".time-block");
   var save = $(".saveBtn");
   var alert = $(".alert");
 
+// Time display functions and variables
   var today = dayjs().format("dddd, MMMM D ");
   $("#currentDay").append(today);
   var currentHour = dayjs().hour();
 
+// Function that displays color styling for hours  
   timeBlock.each(function () {
     var blockValue = $(this).attr("id").slice(5)
     if (blockValue < currentHour) {
@@ -19,6 +21,7 @@ $(function () {
     }
   });
 
+// Function to save information into localStorage with alert message
   save.on("click", function (event) {
     event.preventDefault();
     var key = $(this).parent().attr("id");
@@ -29,11 +32,13 @@ $(function () {
 
     localStorage.setItem(key, value);
 
+// Function to remove alert message
     setTimeout(function() {
       alertMessage.remove();
     }, 1500);
   });
 
+// For loop used to show what was entered in localStorage
   for (var i = 9; i <= 17; i++) {
     $(`#hour-${i} textarea`).val(localStorage.getItem(`hour-${i}`))
   };
